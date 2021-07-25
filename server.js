32c4eedbd222b61dbdb0871f1eccde4811b0670f
Trevor Dolbow <tdolbow@udel.edu>
2021-07-25 19:17:27 -0400

    Added ability for python script to run on connection.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 //var http = require('http');  
 //var fs = require('fs');  
+const {PythonShell} = require('python-shell');
+const {spawn} = require('child_process');
 
+function callScraper(){
+    const python = spawn('python',['/MealScraper/main.py']);
+    python.stdout.on('data', function(data)  {
+        console.log("Getting data from python script");
+        console.log(data);
+    })
+}
 
 var express = require("express");
 var app = new express();
@@ -8,8 +17,31 @@ var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
 app.get('/',function(request,response){
+    console.log("In request");
     response.sendFile("/public/index.html");
+    PythonShell.run(test.py, null,  function (err){
+    if (err){
+        throw err;
+    }
+    console.log('finished');
+
+    });
+
 
 });
 
 app.listen(8000);
+console.log("After listen");
+let options = {
+    mode: 'text',
+    //pythonPath: '',
+    pythonOptions: ['-u'],
+    scriptPath: 'MealScraper/'
+};
+    PythonShell.run("main.py",options,  function (err, results){
+    if (err){
+        throw err;
+    }
+    console.log(results);
+})
+    console.log('finished');
