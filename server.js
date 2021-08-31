a78b80eab7b52cd7c1bed966297976eb250aee52
Trevor Dolbow <tdolbow@udel.edu>
2021-08-31 12:22:16 -0400

    Updated public dir

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 //var http = require('http');  
 //var fs = require('fs');  
-
-
 var express = require("express");
+const {spawn} = require("child_process");
+port = 8000
+
+
+
+
 var app = new express();
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
@@ -12,4 +16,15 @@ app.get('/',function(request,response){
 
 });
 
-app.listen(8000);
+var dataToSend;
+const python = spawn("python", [""]);
+python.stdout.on("data", function (data) {
+    console.log();
+    dataToSend = data.toString();
+});
+
+python.on("close", (code) => {
+    console.log("Child process closed, sending data. ");
+});
+
+app.listen(port);
