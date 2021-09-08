fb3aec6ebbcca49226915155aa582f8960e571de
Noah <noahmhodgson@gmail.com>
2021-09-07 21:00:23 -0400

    star*

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var csv = require('csvtojson');
 var app = express();
 
 var corsMiddleware = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'localhost'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Origin', '*'); //replace localhost with actual host
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     next();
