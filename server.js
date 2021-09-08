05d65eea034a2abfa3772a6399582ec5d2ac96af
Noah <noahmhodgson@gmail.com>
2021-09-07 21:55:14 -0400

    hope is faith pouty, witchier sister

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 var express = require('express');
-var socket = require('socket.io');
 var cors = require('cors')
 var csv = require('csvtojson');
 
 //App setup
 var app = express();
 
-app.use(cors())
-
 var server = app.listen(80, function(){
     console.log("Listening to requests on port 80");
 });
+var io = require('socket.io')(server);
+
 
 //Static files
 app.use(express.static(__dirname + '/public'));
 
 //Socket setup
-var io = socket(server);
 
 io.on('connection', function(socket){
     console.log("Made a socket connection -- Socket id: ", socket.id, " -- IP: ", socket.request.connection.remoteAddress);
