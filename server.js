b0e753839a221c773496ae7439e64b9629aff83a
Noah <noahmhodgson@gmail.com>
2021-09-07 20:58:53 -0400

    trying again

--- server.js
+++ server.js
@@ -6,18 +6,15 @@ var csv = require('csvtojson');
 //App setup
 var app = express();
 
-app.use((req, res, next) => {
-    //allow access from every, elminate CORS
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.removeHeader('x-powered-by');
-    //set the allowed HTTP methods to be requested
-    res.setHeader('Access-Control-Allow-Methods','POST');
-    //headers clients can use in their requests
-    res.setHeader('Access-Control-Allow-Headers','Content-Type');
-    //allow request to continue and be handled by routes
+var corsMiddleware = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'localhost'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     next();
-  });
-  
+}
+
+app.use(corsMiddleware);
+
 var server = app.listen(8000, function(){
     console.log("Listening to requests on port 8000");
 });
