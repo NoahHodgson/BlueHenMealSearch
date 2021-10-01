f4307e8846a9f8e52b86a2382155f33a5323b890
Noah <noahmhodgson@gmail.com>
2021-10-01 15:05:30 -0400

    breakfast lunch and dinner now loading

--- MealScraper/appendToCsv.py
+++ MealScraper/appendToCsv.py
@@ -2,15 +2,15 @@ import csv
 from os import path
 from datetime import datetime
 
-def append(url, menu):
-    if(not path.exists("results.csv")):
-        open("results.csv",'x')
-        with open("results.csv",'a') as csvFile:
+def append(url, menu, file):
+    if(not path.exists(file)):
+        open(file,'x')
+        with open(file,'a') as csvFile:
             writer = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             writer.writerow(["item","url","Date"])
     
     current = datetime.now()
-    with open("results.csv",'a') as csvFile:
+    with open(file,'a') as csvFile:
         writer = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for item in menu:
             writer.writerow([item,url,current])
--- MealScraper/main.py
+++ MealScraper/main.py
@@ -1,15 +1,73 @@
 from appendToCsv import append
 from scraper import printItems, getItems
 import os
+import datetime
 
-dirLoc = os.path.dirname(os.path.abspath(__file__))
-urlLoc = dirLoc + "/urls"
-print(urlLoc)
-f = open(urlLoc,"r")
-urls = f.readlines()
+dt = datetime.datetime.today()
 
-for url in urls:
+#test week day
+if dt.weekday() > 4:
+    cr_breakfast="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1382&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    cr_lunch="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1382&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    cr_dinner="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1384&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+
+
+    pen_breakfast="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1382&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    pen_lunch="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1382&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    pen_dinner="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1384&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+
+    #build arrays
+    breakfast_array=[cr_breakfast, pen_breakfast]
+    lunch_array=[cr_lunch, pen_lunch]
+    dinner_array=[cr_dinner, pen_dinner]
+
+elif dt.weekday() == 4:
+    russ_lunch="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1760&storeIds=&mode=Daily&periodId=1383&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    cr_breakfast="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1382&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    cr_lunch="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1383&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    cr_dinner="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1384&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+
+
+    pen_breakfast="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1382&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    pen_lunch="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1383&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    pen_dinner="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1384&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)  
+
+    #build arrays
+    breakfast_array=[cr_breakfast, pen_breakfast]
+    lunch_array=[russ_lunch, cr_lunch, pen_lunch]
+    dinner_array=[cr_dinner, pen_dinner]
+
+else:
+    russ_lunch="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1760&storeIds=&mode=Daily&periodId=1383&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    russ_dinner="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1760&storeIds=&mode=Daily&periodId=1384&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+
+    cr_breakfast="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1382&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    cr_lunch="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1383&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    cr_dinner="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=14812&storeIds=&mode=Daily&periodId=1384&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+
+
+    pen_breakfast="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1382&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    pen_lunch="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1383&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)
+    pen_dinner="https://udel.campusdish.com/LocationsAndMenus/CaesarRodneyFreshFoodCompany?locationId=1729&storeIds=&mode=Daily&periodId=1384&date="+str(dt.month)+"%2F"+str(dt.day)+"%2F"+str(dt.year)  
+
+    #build arrays
+    breakfast_array=[cr_breakfast, pen_breakfast]
+    lunch_array=[russ_lunch, cr_lunch, pen_lunch]
+    dinner_array=[russ_dinner, cr_dinner, pen_dinner]
+
+
+#for-loop, need to separate for different meal times for different resultfiles
+for url in breakfast_array:
     print("URL is: "+url)
     printItems(url)
-    append(url,getItems(url))
+    append(url,getItems(url),"b_results.csv")
 
+for url in lunch_array:
+    print("URL is: "+url)
+    printItems(url)
+    append(url,getItems(url),"l_results.csv")
+
+for url in dinner_array:
+    print("URL is: "+url)
+    printItems(url)
+    append(url,getItems(url),"d_results.csv")
